78cce5ee5f4653e25fb617b7a43b72f64984af1b
fenq2 <fenq@ukr.net>
2024-07-27 12:46:15 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/sticker.webp" width="200" height="200" />
+  </header>
+</template>
+
+<style scoped>
+header {
+  margin: 0;
+  background: #efeaea;
+  font-size: 100px;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
